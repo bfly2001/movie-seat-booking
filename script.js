f6ef0cf5622b73213085d5f09e9e1cd8baba1f2c
bfly2001 <brendan.sflynn@gmail.com>
2020-08-04 09:47:15 -0700

    Update script.js
    -Added 'setMovieData' function.
    -Added 'seatsIndex' const.
    -Set 'selectedSeats' to local storage.
    -Added setMovieData function to movieSelect event listener.

--- script.js
+++ script.js
@@ -6,10 +6,22 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+//save selected movie and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 //Update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    //copy selected seats into array
+    //map through array
+    //return a new array of indexes
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
     const selectedSeatsCount = selectedSeats.length;
 
@@ -20,6 +32,7 @@ function updateSelectedCount() {
 //movie select event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
 
